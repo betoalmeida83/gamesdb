9ded0f6e5834fac46004de2c22594e2fd773d045
betoalmeida83 <roberto.almeida5@fatec.sp.gov.br>
2025-05-20 12:04:41 -0300

    Project created

---
+++ src/main/java/com/gamesdb/gamesdb/GamesdbApplication.java
@@ -0,0 +1,13 @@
+package com.gamesdb.gamesdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamesdb/gamesdb/GamesdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamesdb.gamesdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
